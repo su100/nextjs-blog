a892ee3be05b5f54f49b5060313df69b21c3583f
su100 <zxctys@naver.com>
2022-07-24 15:25:50 +0900

    feat: 날짜 형식 변경

--- components/date.js
+++ components/date.js
@@ -1,6 +1,23 @@
-import { parseISO, format } from 'date-fns';
+import { format, formatDistanceToNow } from 'date-fns';
+import { ko } from 'date-fns/locale';
 
-export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+function formatDate(date) {
+  if (!date) return;
+  const d = new Date(date);
+  const now = Date.now();
+  const diff = (now - d.getTime()) / 1000;
+
+  if (diff < 60 * 1) {
+    // 1분 미만
+    return '방금 전';
+  }
+  if (diff < 60 * 60 * 24 * 3) {
+    // 3일 미만: 몇시간 전, 몇일 전
+    return formatDistanceToNow(d, { addSuffix: true, locale: ko });
+  }
+  return format(d, 'PPP EEE p', { locale: ko }); // PPP: 날짜, EEE: 요일, p: 시간
+}
+
+export default function FormatDate({ dateString }) {
+  return <time dateTime={dateString}>{formatDate(dateString)}</time>;
 }
